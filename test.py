d5add0dde4c8b9d6cbd4d2520d7549f4f6387f1e
KorvoDron <gentlemancoderman@gmail.com>
2024-09-16 11:14:06 +0300

    asdf

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 from PIL import Image
 import matplotlib.pyplot as plt
+import os
 
 def test(name):
-    img = Image.open(f"memes/{name}")
+    img = Image.open(f"{os.path.dirname(__file__)}/memes/{name}")
     plt.imshow(img)
     plt.axis('off')
     plt.show()
