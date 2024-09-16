90517c7dcc813f526602575c7db42d6104134c2d
KorvoDron <gentlemancoderman@gmail.com>
2024-09-16 11:12:34 +0300

    ASF

--- test.py
+++ test.py
@@ -2,7 +2,9 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 def test(name):
-    img = Image.open(f"memes/{name}.png")
+    img = Image.open(f"memes/{name}")
     plt.imshow(img)
     plt.axis('off')
     plt.show()
+
+test('i.png')
