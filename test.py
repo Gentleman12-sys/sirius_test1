40809c05df85da9ba25fd64578c6e9763e9d51b2
KorvoDron <gentlemancoderman@gmail.com>
2024-09-16 10:43:31 +0300

    Changes to be committed: 	modified:   test.py

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-for i in range(1, 100):
-    print(i)
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(host=8000)
